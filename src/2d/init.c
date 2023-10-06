440f88837ee58fe368bf455da4a77ee60886bfe4
islem kaismoun <islem@islem.com>
2023-10-06 22:47:49 +0200

    ajout ray casting pas opti

--- src/2d/init.c
+++ src/2d/init.c
@@ -12,6 +12,12 @@
 
 #include "../../includes/cub3d.h"
 
+void    my_mlx_pixel_put(t_winmlx *draw, int x, int y, int color)
+{
+    if ((x >= 0 && x < 900) && (y >= 0 && y < 900))
+        ((int *)draw->addr)[y * (draw->line_length >> 2) + x] = color;
+}
+
 void reset_player_position_on_map(t_data *data)
 {
     int i = 0;
@@ -23,13 +29,11 @@ void reset_player_position_on_map(t_data *data)
         while (data->head_file->map[i][j])
         {
             if (data->head_file->map[i][j] == 'N')
-                data->head_file->map[i][j] = '0'; // Réinitialisez la position précédente du joueur à une case vide
+                data->head_file->map[i][j] = '0';
             j++;
         }
         i++;
     }
-
-    // Mettez à jour la nouvelle position du joueur sur la carte
     data->head_file->map[data->head_player->posy / 30][data->head_player->posx / 30] = 'N';
 }
 
@@ -45,47 +49,160 @@ void rotate_player(t_data *data, int direction)
 	printf("OH%d\n", data->head_player->angle);
 }
 
+bool is_collision(t_data *data, int newX, int newY)
+{
+    int mapX = newX / 30;
+    int mapY = newY / 30;
+
+    // Vérifier si la position est à l'intérieur d'une cellule de mur
+    if (data->head_file->map[mapY][mapX] == '1')
+        return true;
+
+    return false;
+}
+
 int ft_key_hook(int keycode, t_data *data)
 {
-	(void)data;
-	printf("%d\n", keycode);
-	if (keycode == 65307)
-	{
-		printf("salut");
-	}
-    else if (keycode == 65362) // haut
+    if (keycode == 65362) // haut
     {
         float moveX = 5 * cos(data->head_player->angle * M_PI / 180);
         float moveY = 5 * sin(data->head_player->angle * M_PI / 180);
-        data->head_player->posx += moveX;
-        data->head_player->posy += moveY;
-        printf("posx = %d\n", data->head_player->posx);
-        printf("posy = %d\n", data->head_player->posy);
+
+        int newX = data->head_player->posx + moveX;
+        int newY = data->head_player->posy;
+
+        // Vérifiez d'abord le mouvement en X
+        if (!is_collision(data, newX, newY))
+            data->head_player->posx = newX;
+
+        newX = data->head_player->posx;  // Mettez à jour la valeur de newX après le mouvement
+        newY = data->head_player->posy + moveY;
+
+        // Vérifiez ensuite le mouvement en Y
+        if (!is_collision(data, newX, newY))
+            data->head_player->posy = newY;
+
+    }
+    else if (keycode == 65364) // bas
+    {
+        float moveX = -2 * cos(data->head_player->angle * M_PI / 180);
+        float moveY = -2 * sin(data->head_player->angle * M_PI / 180);
+
+        int newX = data->head_player->posx + moveX;
+        int newY = data->head_player->posy;
+
+        if (!is_collision(data, newX, newY))
+            data->head_player->posx = newX;
+
+        newX = data->head_player->posx;
+        newY = data->head_player->posy + moveY;
+
+        if (!is_collision(data, newX, newY))
+            data->head_player->posy = newY;
     }
 	else if (keycode == 65363) // droite
-	{
 		rotate_player(data, 1);
-
-	}
-	else if (keycode == 65364) // bas
-	{
-		data->head_player->posy += 2;
-	}
 	else if (keycode == 65361) // gauche
-	{
 		rotate_player(data, -1);
-
-	}
-	printf("%d\n", data->head_player->posy);
-	printf("%d\n", data->head_player->posx);
 	return (0);
 }
 
-void    my_mlx_pixel_put(t_winmlx *draw, int x, int y, int color)
-{
-    if ((x >= 0 && x < 900) && (y >= 0 && y < 900))
-        ((int *)draw->addr)[y * (draw->line_length >> 2) + x] = color;
+void drawplayer(t_data *data, int posx, int posy) {
+    int i = 0;
+    int j;
+
+    int squareSize = 10;  // Taille du carré du joueur
+    int halfSize = squareSize / 2;  // Demi-taille pour le centrage
+
+    int saveposx = posx - halfSize;  // Position initiale ajustée au centre horizontalement
+    int saveposy = posy - halfSize;  // Position initiale ajustée au centre verticalement
+
+    while (i < squareSize) {
+        j = 0;
+        posx = saveposx;
+        while (j < squareSize) {
+            my_mlx_pixel_put(data->head_winmlx, posx, saveposy + i, H_YELLOW);
+            j++;
+            posx++;
+        }
+        posy++;
+        i++;
+    }
 }
+
+
+void draw_line(t_data *data, int x1, int y1, int x2, int y2, int color) {
+    // Algorithme de tracé de ligne (Bresenham)
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int sx = (x1 < x2) ? 1 : -1;
+    int sy = (y1 < y2) ? 1 : -1;
+    int err = dx - dy;
+
+    while (1) {
+        my_mlx_pixel_put(data->head_winmlx, x1, y1, color);
+
+        if (x1 == x2 && y1 == y2) {
+            break;
+        }
+
+        int err2 = 2 * err;
+        if (err2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+        if (err2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
+}
+
+
+void cast_rays(t_data *data) {
+    int num_rays = 30;  // Nombre de rayons à lancer
+    float fov = 60.0;   // Champ de vision en degrés
+    float angle_increment = fov / num_rays;
+
+    for (int i = 0; i < num_rays; i++) {
+        // Calcule l'angle du rayon en fonction du champ de vision et de l'itération
+        float ray_angle = data->head_player->angle - (fov / 2) + i * angle_increment;
+
+        // Convertit l'angle en radians
+        float ray_angle_rad = ray_angle * M_PI / 180.0;
+
+        // Initialise les coordonnées du rayon à la position du joueur
+        float ray_x = data->head_player->posx;
+        float ray_y = data->head_player->posy;
+
+        // Initialise les coordonnées du point d'impact potentiel
+        float hit_x = ray_x;
+        float hit_y = ray_y;
+
+        // Avance le rayon jusqu'à ce qu'il atteigne une nouvelle case
+		while (1) {
+			// Obtient les indices de la case actuelle dans la grille
+			int map_x = (int)(hit_x / 30);
+			int map_y = (int)(hit_y / 30);
+
+			// Vérifie si le rayon est sorti de la grille
+
+			// Vérifie si la case actuelle est un mur
+			if (data->head_file->map[map_y][map_x] == '1') {
+				// Dessine une ligne du joueur au point de collision avec le mur
+				draw_line(data, data->head_player->posx, data->head_player->posy, (int)hit_x, (int)hit_y, H_RED);
+				break;  // Sort de la boucle interne
+			}
+
+			hit_x += cos(ray_angle_rad) * 0.1;
+			hit_y += sin(ray_angle_rad) * 0.1;
+		}
+    }
+}
+
+
+
+
 
 void drawsquare(t_data *data, int color, int x, int y)
 {
@@ -98,7 +215,10 @@ void drawsquare(t_data *data, int color, int x, int y)
 		x = save;
 		while (j < 30)
 		{
-			my_mlx_pixel_put(data->head_winmlx, x, y, color);
+			if (j == 0 || j == 29 || i == 29 || i == 0)
+				my_mlx_pixel_put(data->head_winmlx, x, y, H_GREY);
+			else
+				my_mlx_pixel_put(data->head_winmlx, x, y, color);
 			j++;
 			x++;
 		}
@@ -107,98 +227,41 @@ void drawsquare(t_data *data, int color, int x, int y)
 	}
 }
 
-int only_space(char *str)
+void drawmap(t_data *data)
 {
-	while (*str)
-	{
-		if (*str != 'x')
-			return (0);
-		str++;
-	}
-	return (1);
-}
-
-void draw_line(t_data *data, int x0, int y0, int x1, int y1, int color)
-{
-    int dx = abs(x1 - x0);
-    int dy = abs(y1 - y0);
-
-    int sx = x0 < x1 ? 1 : -1;
-    int sy = y0 < y1 ? 1 : -1;
-
-    int err = (dx > dy ? dx : -dy) / 2;
-    int e2;
-
-    while (1)
-    {
-        my_mlx_pixel_put(data->head_winmlx, x0, y0, color);  // Dessinez le pixel à cette position
-
-        if (x0 == x1 && y0 == y1)
-            break;
-        e2 = err;
-        if (e2 > -dx)
-        {
-            err -= dy;
-            x0 += sx;
-        }
-        if (e2 < dy)
-        {
-            err += dx;
-            y0 += sy;
-        }
-    }
-}
-
-void draw_arrow(t_data *data, int x, int y, float angle)
-{
-    int tipX = x + (10 * cos(angle));  // Calculez la position de la pointe de la flèche
-    int tipY = y + (10 * sin(angle));
-
-    int wing1X = tipX - (5 * cos(angle + M_PI / 4));  // Calculez la position de la première aile
-    int wing1Y = tipY - (5 * sin(angle + M_PI / 4));
-
-    int wing2X = tipX - (5 * cos(angle - M_PI / 4));  // Calculez la position de la deuxième aile
-    int wing2Y = tipY - (5 * sin(angle - M_PI / 4));
-
-    draw_line(data, x, y, tipX, tipY, H_BLACK);  // Dessinez la tige
-    draw_line(data, tipX, tipY, wing1X, wing1Y, H_BLACK);  // Dessinez la première aile
-    draw_line(data, tipX, tipY, wing2X, wing2Y, H_BLACK);  // Dessinez la deuxième aile
-}
-
-
-
-static int	random_next_frame(t_data *data)
-{
-	int i = 0;
-	int j;
-	int x = 0;
+    int x = 0;
 	int y = 0;
-    reset_player_position_on_map(data);
-	data->head_winmlx->img = mlx_new_image(data->head_winmlx->mlx, 900, 900);
-	data->head_winmlx->addr = mlx_get_data_addr(data->head_winmlx->img, &data->head_winmlx->bits_per_pixel, &data->head_winmlx->line_length, &data->head_winmlx->endian);
-	// mlx_clear_window(data->head_winmlx->mlx, data->head_winmlx->mlx_win);
-	while(data->head_file->map[i])
+    int i = 0;
+	int j;
+    while(data->head_file->map[i])
 	{
 		j = 0;
 		x = 0;
 		while (data->head_file->map[i][j])
 		{
 			if (data->head_file->map[i][j] == '1')
-				drawsquare(data, H_RED, x, y);
+				drawsquare(data, H_BLACK, x, y);
 			else if (data->head_file->map[i][j] == '0')
-				drawsquare(data, H_GREEN, x, y);
+				drawsquare(data, H_WHITE, x, y);
 			else if (data->head_file->map[i][j] == 'N')
-			{
 				drawsquare(data, H_PINK, x, y);
-				// my_mlx_pixel_put(data->head_winmlx, data->head_player->posx , data->head_player->posy, H_BLACK);
-			}
 			x+=30;
 			j++;
 		}
 		y+=30;
 		i++;
 	}
-    draw_arrow(data, data->head_player->posx, data->head_player->posy, data->head_player->angle * M_PI / 180);
+}
+
+static int	random_next_frame(t_data *data)
+{
+
+    reset_player_position_on_map(data);
+	data->head_winmlx->img = mlx_new_image(data->head_winmlx->mlx, 1920, 1080);
+	data->head_winmlx->addr = mlx_get_data_addr(data->head_winmlx->img, &data->head_winmlx->bits_per_pixel, &data->head_winmlx->line_length, &data->head_winmlx->endian);
+    drawmap(data);
+	drawplayer(data, data->head_player->posx, data->head_player->posy);
+	cast_rays(data);
 	mlx_put_image_to_window(data->head_winmlx->mlx, data->head_winmlx->mlx_win, data->head_winmlx->img, 0, 0);
 	mlx_destroy_image(data->head_winmlx->mlx, data->head_winmlx->img);
 	return (0);
@@ -230,18 +293,11 @@ int get_pos(int mod, char **map)
 
 int ft_init(t_winmlx *winmlx, t_data *data)
 {
-	(void)data;
-	data->head_player = malloc(sizeof(t_player));
 	data->head_player->angle = 400;
-	data->head_player->posx = 10;
-	data->head_player->posy = 10;
-    data->head_player->posx = get_pos(1, data->head_file->map) * 30;
-	data->head_player->posy = get_pos(0, data->head_file->map) * 30;
-	data->head_player->startposx = data->head_player->posx;
-	data->head_player->startposy = data->head_player->posy;
+    data->head_player->posx = (get_pos(1, data->head_file->map) * 30) + 10;
+	data->head_player->posy = (get_pos(0, data->head_file->map) * 30) + 10;
 	winmlx->mlx = mlx_init();
-	winmlx->mlx_win = mlx_new_window(winmlx->mlx, 900, 900, "Cub3d");
-	// load_img(winmlx);
+	winmlx->mlx_win = mlx_new_window(winmlx->mlx, 1920, 1080, "Cub3d");
 	mlx_hook(data->head_winmlx->mlx_win, 2, 1L << 0, ft_key_hook, data);
 	mlx_loop_hook(winmlx->mlx, random_next_frame, data);
 	mlx_loop(winmlx->mlx);
--- src/main.c
+++ src/main.c
@@ -13,6 +13,16 @@
 #include "../includes/cub3d.h"
 
 
+void init_struct(t_data *data)
+{
+	data->head_file = ft_calloc(1, sizeof(t_file));
+	ft_bzero(data->head_file, sizeof(t_file));
+	data->head_player = ft_calloc(1, sizeof(t_player));
+	ft_bzero(data->head_player, sizeof(t_player));
+	data->head_winmlx = ft_calloc(1, sizeof(t_winmlx));
+	ft_bzero(data->head_winmlx, sizeof(t_winmlx));
+}
+
 
 int	main(int argc, char **argv)
 {
@@ -20,79 +30,11 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 		return (ft_putstr_fd("Error Number of Args\n", 2), 1);
-	data.head_file = malloc(sizeof(t_file));
+	init_struct(&data);
 	if (parsing(argv[1], &data))
 		return (free(data.head_file), 1);
-	data.head_winmlx = malloc(sizeof(t_winmlx));
-	int i = 0;
-	while (data.head_file->map[i])
-	{
-		printf("%s\n", data.head_file->map[i]);
-		i++;
-	} 	
 	ft_init(data.head_winmlx, &data);
 	// ft_free_tab(data.head_file->map);
 	// free(data.head_file);
 	// free(data.head_winmlx);
 }
-
-
-// typedef struct  s_test
-// {
-//     int x;
-//     int y;
-//     int angle;
-// }               t_test;
-
-// void rotate_player(t_test *player, int direction)
-// {
-//     player->angle += direction * 5;
-//     if (player->angle > 360)
-//         player->angle -= 360;
-//     else if (player->angle < 0)
-//         player->angle += 360;
-// }
-
-
-// int key_hook(int keycode, t_test *player)
-// {
-//     if (keycode == 123) // Left arrow key
-//         rotate_player(player, -1);
-//     else if (keycode == 124) // Right arrow key
-//         rotate_player(player, 1);
-//     return (0);
-// }
-
-
-
-// int draw_player(t_test *player, void *mlx, void *win)
-// {
-// 	int x = player->x + cos(player->angle * M_PI / 180) * 10;
-// 	int y = player->y + sin(player->angle * M_PI / 180) * 10;
-// 	mlx_pixel_put(mlx, win, x, y, 0x00FF0000);
-// 	return (0);
-// }
-
-// int main(void)
-// {
-//     t_test player;
-//     void *mlx;
-//     void *win;
-
-// 	player.x = 200;
-// 	player.y = 100;
-// 	player.angle = 20;
-//     mlx = mlx_init();
-// 	if (mlx == NULL) {
-//    		 printf("Failed to initialize MiniLibX\n");
-// 		return 1;
-// 	}
-//     win = mlx_new_window(mlx, 800, 600, "Player Rotation");
-// 	if (win == NULL) {
-// 		printf("Failed to create window\n");
-// 		return 1;
-// 	}
-//     mlx_hook(win, 2, 1L << 0, key_hook, &player);
-//     mlx_loop_hook(mlx, draw_player, &player);
-//     mlx_loop(mlx);
-// }
