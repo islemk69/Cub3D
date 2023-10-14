5b06227b72df0720f5a7de2ecd6cae0aed31b57b
Islem Kaismoun <ikaismou@z3r7p5.42lyon.fr>
2023-10-14 18:37:25 +0200

    minimap beta

--- src/game/drawmap.c
+++ src/game/drawmap.c
@@ -6,7 +6,7 @@
 /*   By: ikaismou <ikaismou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 15:46:36 by ikaismou          #+#    #+#             */
-/*   Updated: 2023/10/14 17:43:49 by ikaismou         ###   ########.fr       */
+/*   Updated: 2023/10/14 18:37:04 by ikaismou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,28 +57,30 @@ void	drawsquare(t_data *data, int color, int x, int y)
 	}
 }
 
-void	drawplayer(t_data *data, int posx, int posy)
+void drawplayer(t_data *data)
 {
-	int	i;
-	int	j;
-	int	saveposx;
-	int	saveposy;
+    int playerSize = 10;
+    int squareSize = 30;
+    int centerX = (data->file->wmap / 2) * squareSize;  // Centre X de la minimap
+    int centerY = (data->file->hmap / 2) * squareSize;  // Centre Y de la minimap
 
-	saveposx = posx - 10 / 2;
-	saveposy = posy - 10 / 2;
-	i = -1;
-	while (++i < 10)
-	{
-		j = -1;
-		posx = saveposx;
-		while (++j < 10)
-		{
-			my_mlx_pixel_put(data->winmlx, posx, saveposy + i, H_YELLOW);
-			posx++;
-		}
-		posy++;
-	}
+    // Ajoutez les coordonnées de la caméra pour ajuster la position du joueur
+    int x = centerX - playerSize / 2 + (data->player->px - (int)data->player->px) * squareSize;
+    int y = centerY - playerSize / 2 + (data->player->py - (int)data->player->py) * squareSize;
+
+    int i, j;
+    for (i = 0; i < playerSize; i++)
+    {
+        int saveX = x;
+        for (j = 0; j < playerSize; j++)
+        {
+            my_mlx_pixel_put(data->winmlx, saveX, y, H_YELLOW);
+            saveX++;
+        }
+        y++;
+    }
 }
+
 
 int	is_player(char p)
 {
@@ -87,28 +89,35 @@ int	is_player(char p)
 	return (0);
 }
 
-void	drawmap(t_data *data, int x, int y)
+void drawmap(t_data *data)
 {
-	int	i;
-	int	j;
+	int mapSize = 10;
+	int squareSize = 30;
+    int startX = (int)data->player->px / squareSize - mapSize / 2;
+    int startY = (int)data->player->py / squareSize - mapSize / 2;
 
-	i = -1;
-	while (data->file->map[++i])
-	{
-		j = -1;
-		x = 0;
-		while (data->file->map[i][++j])
-		{
-			if (data->file->map[i][j] == '1')
-				drawsquare(data, H_BLACK, x, y);
-			else if (data->file->map[i][j] == '0')
-				drawsquare(data, H_WHITE, x, y);
-			else if (is_player(data->file->map[i][j]))
-				drawsquare(data, H_PINK, x, y);
-			x += 30;
-		}
-		y += 30;
-	}
-	reset_player_position_on_map(data);
-	drawplayer(data, data->player->px, data->player->py);
+    if (startX < 0)
+        startX = 0;
+    if (startY < 0)
+        startY = 0;
+
+    int i, j, x, y;
+    y = 0;
+    for (i = startY; i < startY + mapSize && i < data->file->hmap; i++)
+    {
+        x = 0;
+        for (j = startX; j < startX + mapSize && j < data->file->wmap; j++)
+        {
+            if (data->file->map[i][j] == '1')
+                drawsquare(data, H_BLACK, x, y);
+            else if (data->file->map[i][j] == '0')
+                drawsquare(data, H_WHITE, x, y);
+            else if (is_player(data->file->map[i][j]))
+               	drawsquare(data, H_PINK, x, y);
+            x += squareSize;
+        }
+        y += squareSize;
+    }
+    reset_player_position_on_map(data);
+	drawplayer(data);
 }
