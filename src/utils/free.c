811db5f074bee52abfc560fe3a4d1fac70ed6c41
Islem Kaismoun <ikaismou@z4r6p3.42lyon.fr>
2023-10-17 17:58:13 +0200

    correction free.c

--- src/utils/free.c
+++ src/utils/free.c
@@ -6,7 +6,7 @@
 /*   By: ikaismou <ikaismou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 16:55:37 by ikaismou          #+#    #+#             */
-/*   Updated: 2023/10/16 16:15:06 by ikaismou         ###   ########.fr       */
+/*   Updated: 2023/10/17 17:50:19 by ikaismou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,22 +53,21 @@ void	free_struct(t_data *data)
 
 void	free_mlx(t_data *data, int flg)
 {
-
-	if (data->ntex && data->ntex->img)
-		mlx_destroy_image(data->winmlx->mlx, data->ntex->img);
-	if (data->stex && data->stex->img)
-		mlx_destroy_image(data->winmlx->mlx, data->stex->img);
-	if (data->wtex && data->wtex->img)
-		mlx_destroy_image(data->winmlx->mlx, data->wtex->img);
-	if (data->etex && data->etex->img)
-		mlx_destroy_image(data->winmlx->mlx, data->etex->img);
-	if (data->winmlx->img && flg)
-		mlx_destroy_image(data->winmlx->mlx, data->winmlx->img);
 	if (data->winmlx)
 	{
-		if (data->ntex && data->winmlx->mlx_win)
+		if (data->ntex && data->ntex->img)
+			mlx_destroy_image(data->winmlx->mlx, data->ntex->img);
+		if (data->stex && data->stex->img)
+			mlx_destroy_image(data->winmlx->mlx, data->stex->img);
+		if (data->wtex && data->wtex->img)
+			mlx_destroy_image(data->winmlx->mlx, data->wtex->img);
+		if (data->etex && data->etex->img)
+			mlx_destroy_image(data->winmlx->mlx, data->etex->img);
+		if (data->winmlx->img && flg)
+			mlx_destroy_image(data->winmlx->mlx, data->winmlx->img);
+		if (data->winmlx->mlx_win)
 			mlx_destroy_window(data->winmlx->mlx, data->winmlx->mlx_win);
-		if (data->ntex && data->winmlx->mlx)
+		if (data->winmlx->mlx)
 			mlx_destroy_display(data->winmlx->mlx);
 		if (data->winmlx->mlx)
 			free(data->winmlx->mlx);
